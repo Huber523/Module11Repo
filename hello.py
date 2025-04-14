b8c02b49377883d59929c5a2d02141687ecfeaf5
Huber523 <morechristian9@gmail.com>
2025-04-14 19:30:46 -0400

    Added new message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Firstname Lastname
+# Christian Marin
 # 4/14/2025
 # A sample Python program to print a message
 
+# Christian Marin
+# 4/14/25
+# added new message
+
 print ("Hello, World!")
+
+print ("Welcome to GitHub!")
